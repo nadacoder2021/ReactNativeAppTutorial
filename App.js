b27bd6c838578664a19f38eabfb61613093d3479
Nada <Nada@Moazzams-iMac.broadband>
2022-02-16 15:33:49 +0000

    First try

--- App.js
+++ App.js
@@ -1,20 +1,73 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  Image,
+  TouchableWithoutFeedback,
+  TouchableOpacity,
+  TouchableHighlight,
+  Button,
+  Alert,
+} from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text
+        style={{
+          fontFamily: "sans-serif",
+          fontSize: "40px",
+          textAlign: "center",
+        }}
+        numberOfLines={2}
+      >
+        {" "}
+        Asalamu alaikum Huzaifah
+      </Text>
+
+      <TouchableHighlight
+        onPress={() => {
+          console.log("Image touched");
+        }}
+      >
+        <Image
+          style={{
+            resizeMode: "cover",
+            height: 200,
+            width: 300,
+            borderRadius: 75,
+          }}
+          source={require("./assets/huzaifah.jpg")}
+          // source={{
+          //
+          //   uri: "https://thequranclasses.online/wp-content/uploads/2020/12/Learn-Quran-for-Kids-The-Quran-Classes.png",
+          // }}
+        />
+      </TouchableHighlight>
+      <Button
+        title="Log in"
+        onPress={() =>
+          Alert.prompt("Mom says:", "You've done it", (text) =>
+            console.log(text)
+          )
+        }
+      />
+      {/* <StatusBar style="auto" /> */}
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#ef233c",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
+
+// react native does not have div, html element, paragraph, have to
+//building blocks used by react native.
